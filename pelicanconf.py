1fcb22348991c5eca980ba5a91016e7d8393f400
dingjiangfeng <dingjiangfeng@beyondsoft.com>
2014-10-17 14:47:30 +0800

    add tao code

--- pelicanconf.py
+++ pelicanconf.py
@@ -66,6 +66,7 @@ LINKS = (('Github', 'https://github.com/dragondjf'),
 
 # Social widget
 SOCIAL = (('Google', 'http://wen.lu/'),
+          ('taocode', 'http://code.taobao.org/'),
           ('百度FEX', 'http://fex.baidu.com/'),
           ('百度EFE', 'http://efe.baidu.com/'),
           ('腾讯AlloyTeam', 'http://www.alloyteam.com/'),
